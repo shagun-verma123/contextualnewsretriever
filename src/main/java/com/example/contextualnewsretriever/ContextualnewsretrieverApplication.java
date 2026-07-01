fa40465f091c6d273787815d602f0e138928ab04
shagunverma <shagun.verma@moonshinetechnology.com>
2026-07-01 13:12:56 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/contextualnewsretriever/ContextualnewsretrieverApplication.java
@@ -0,0 +1,13 @@
+package com.example.contextualnewsretriever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextualnewsretrieverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContextualnewsretrieverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/contextualnewsretriever/ContextualnewsretrieverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.contextualnewsretriever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextualnewsretrieverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
